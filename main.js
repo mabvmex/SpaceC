4bcb7cac2e187f380fd349a2f17b9a63c4721cfa
mabvmex <mabvmex@gmail.com>
2018-06-26 10:07:22 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,30 @@
+//1. CANVAS
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+//2. CONSTANTES
+var interval;
+var cuadros;
+var imagenes = {
+fondo = "./images/bg1"
+}
+
+
+//3. CLASES
+class tablero (){
+    constructor ();
+    this.x = 0;
+    this.y = 0;
+    this.width = canvas.width;
+    this.heigh = canvas.height;
+
+    
+}
+//4. INSTANCIAS
+
+//5. FUNCIONES PRINCIPALES
+
+//6. FUNCIONES AUXILIARES
+
+//7. LISTENERS
+
