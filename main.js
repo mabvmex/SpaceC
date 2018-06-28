53ab3a92e4adec1a7e178bac8498606ec39c8782
mabvmex <mabvmex@gmail.com>
2018-06-27 21:09:19 -0500

    Miercoles - funcionando

--- main.js
+++ main.js
@@ -5,23 +5,19 @@ var ctx = canvas.getContext('2d');
 //2. CONSTANTES
 var interval;
 var cuadros;
-var fire = [];
 var sonidos = {
     ost: ('./sounds/koc.mp3'),
 }
 var imagenes = {
-    human: ('./images/Characters/Humanity/Human-fighter_1.png'),   
+    human: ('./images/Characters/Humanity/Human-fighter_3.png'),   
+    alien: ('./images/Characters/Covenant/Alien-spacecraft_3.png'),
     humanfireball: ('./images/Characters/Humanity/Human-fireball.png'),
-    alien: ('./images/Characters/Covenant/Alien-spacecraft_1.png'),
     alienfireball: ('./images/Characters/Covenant/Alien-fireball.png'),
     fondo: ('./images/bg/Outer Space.jpg')
-    //fondo: ('./images/bg/bg-1.png')
-    //fondo: ('./images/bg/space-artworkjpg.jpg')
 };
 var sound = new Audio();
     sound.src = sonidos.ost;
     sound.loop = true;
-
 
 
 //3. CLASES
@@ -44,14 +40,50 @@ class Mapa {
         ctx.drawImage(this.image, this.x + this.width,this.y,this.width,this.height); //Dibujate en 'X' después de ti misma con la misma altura y misma longitud
     }
 } //Termina la clase MAPA
-      
+
+class Bala {
+    constructor(character){
+        this.y = character.y;
+        this.width = 50;
+        this.height = 50;
+        this.x = character.x + this.width;
+        this.vX = 20;
+        this.image = new Image;
+        this.image.src = imagenes.humanfireball;
+        this.image.onload = function(){
+            this.draw();
+        }.bind(this)
+}
+draw(){
+    this.x += this.vX;
+    ctx.drawImage(this.image, this.x, this.y, this.width,this.height);
+}
+}//Termina constructor de Bala
+
+class BalaAlien {
+    constructor(character){
+        this.y = character.y;
+        this.width = 50;
+        this.height = 50;
+        this.x = character.x - this.width;
+        this.vX = -20;
+        this.image = new Image;
+        this.image.src = imagenes.alienfireball;
+}
+draw(){
+    this.x += this.vX;
+    ctx.drawImage(this.image, this.x, this.y, this.width,this.height);
+}
+}//Termina constructor de BalaAlien
+
 
 class Human {
     constructor(){
-        this.width = 50;
-        this.height = 50;
+        this.width = 60;
+        this.height = 60;
         this.x = 10;
         this.y = ((canvas.height/2) - (this.height/2)); // Posición inicial en el centro del mapa.
+        this.balas = []; 
         this.image = new Image();
         this.image.src = imagenes.human;
         this.image.onload = function(){
@@ -65,30 +97,13 @@ class Human {
 
 } //Termina la clase Human
 
-    class HumanFireball {
-        constructor(){
-            this.x = human.x + human.width; //posición automática delante de Character
-            this.y = human.y;
-            this.width = 40;
-            this.height = 50;
-            this.image = new Image();
-            this.image.src = imagenes.humanfireball;
-            this.image.onload = function(){
-                this.draw();
-            }.bind(this)
-        }
-        draw(){
-            this.x, this.y;
-            ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
-        }
-    } //Termina la clase Humanfireball
-
     class Alien {
         constructor(){
-            this.x = canvas.width-100; // 964;//canvas.width - 100;//
-            this.y = 235;
-            this.width = 50;
-            this.height = 50;
+            this.width = 60;
+            this.height = 60;
+            this.x = canvas.width-100;
+            this.y = ((canvas.height/2)-(this.height/2));
+            this.balasAlien = []; 
             this.image = new Image();
             this.image.src = imagenes.alien;
             this.image.onload = function(){
@@ -101,117 +116,129 @@ class Human {
         }
     } //Termina la clase Alien
 
-    class AlienFireball {
-        constructor(){
-            this.x = 850; //this.x + human.x posicion automatica delante del human
-            this.y = 35;
-            this.width = 30;
-            this.height = 40;
-            this.image = new Image();
-            this.image.src = imagenes.alienfireball;
-            this.image.onload = function(){
-                this.draw();
-            }.bind(this)
-        }
-        draw(){
-            this.x, this.y;
-            ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
-        }
-    } //Termina la clase AlienFireball
-
-
 //4. INSTANCIAS
 var mapa = new Mapa();
 var human = new Human();
-var humanFireball = new HumanFireball();
 var alien = new Alien();
-var alienFireball = new AlienFireball();
+//var alienfireball = new AlienFireball();
+//var humanfireball = new HumanFireball();
+
 
 //5. FUNCIONES PRINCIPALES
 function update(){
-    cuadros++;
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+    if(cuadros < 30 )cuadros++;
+    ctx.clearRect(0,0,canvas.width,canvas.height); 
     mapa.draw();
     human.draw();
-    humanFireball.draw();
     alien.draw();
-    alienFireball.draw();
+    drawDisparo();
+    drawDisparoAlien();
+    
 }
 
 function start(){
-    if (interval) return;
+   // if (interval) return; <-- Para que se usó?
     interval = setInterval(update, 1000/60);
     sound.play()
-
 }
 
-//6. FUNCIONES AUXILIARES
-function disparar(){
-    var fire = new Fuego(human);   //<-- Es el disparo -->
-    human.humanFireball.push(fire);    // fire es el arreglo donde se guardan los diaparos.
+//6. FUNCIONES AUXILIARESv
+function crearDisparo(){
+    var humanfireball = new Bala(human);
+    human.balas.push(humanfireball);   
 }
-
 function drawDisparo(){
-    human.humanFireball.forEach(function(d){
-        d.draw();
+    human.balas.forEach(function(humanfireball){
+        humanfireball.draw();
+    });
+}
+//  *  *  * ALIEN SIDE  *  *  *
+function crearDisparoAlien(){
+    var alienfireball = new BalaAlien(alien);
+    alien.balasAlien.push(alienfireball);   
+}
+function drawDisparoAlien(){
+        alien.balasAlien.forEach(function(alienfireball){
+        alienfireball.draw();
     });
 }
 
 //7. LISTENERS
-addEventListener('keydown', function (e){
-    switch(e.keyCode) {
-
-    //     if (mario.x === 0) return //Si mario llegó a cero, ya no restes (return)
-    //     mario.x -= 64;
-    //     break;
-    // case 39://Right arrow key
-    // if (mario.x === canvas.width - mario.width) return 
-    //     mario.x += 64;
-    //     break;
-
-
-        case 38: //ArrowUp
-            if (human.y - (human.height/2 + 10)  < 0) return;    // Si la posición en 'Y' de 'humano' menos la altura propia de 'humano'
-            human.y -=30;   
-            sound.play()                       // ES MAYOR QUE cero, entonces ya no avances hacia arriba;
-                break;
-        case 40: //ArrowDown
+addEventListener('keydown', function (h){
+    switch(h.keyCode) {
+        case 87:                                                                //ArrowUp
+            if (human.y - (human.height/2 + 10)  < 0) return;                   // Si la posición en 'Y' de 'humano' menos la altura propia de 'humano'
+            human.y -=30;                                                       // ES MAYOR QUE cero, entonces ya no avances hacia arriba;
+            break;
+        case 83:                                                                //ArrowDown
             if (human.y + human.height/2 + 50> (canvas.height)) return;
-            console.log(canvas.height)
-                human.y += 30;
-                break;
-        case 76:  //KeyL
-            console.log("Disparando con L");
-            disparar();
-            drawDisparo();
+            human.y += 30;
             break;
-           
-            //ACCIONES PARA ALIEN/UP2
-        case 87: //KeyW -> UP
-        if (alien.y - (alien.height/2 + 10)  < 0) return;    // Si la posición en 'Y' de 'humano' menos la altura propia de 'humano'
-        alien.y -=30;                          // ES MAYOR QUE cero, entonces ya no avances hacia arriba;
+        case 70:                                                                //KeyL
+            crearDisparo();
             break;
 
-            if (alien.y - alien.height < 0) return; // Si la posición en 'Y' de 'alien' menos la altura propia de 'alien'    
-            alien.y -= 100;                         // ES MAYOR QUE cero, entonces ya no avances hacia arriba;
+//  *  *  * ACCIONES PARA ALIEN SIDE  *  *  *
+        case 38:                                                                // KeyW -> UP
+            if (alien.y - (alien.height/2 + 10)  < 0) return;                   // Si la posición en 'Y' de 'humano' menos la altura propia de 'humano'
+            alien.y -=30;                                                       // ES MAYOR QUE cero, entonces ya no avances hacia arriba;
+            // break;
+            // if (alien.y - alien.height < 0) return;                          // Si la posición en 'Y' de 'alien' menos la altura propia de 'alien'    
+            //alien.y -= 30;                                                    // ES MAYOR QUE cero, entonces ya no avances hacia arriba;
             break;
-        case 83: //KeyS -> Down
-        if (alien.y + alien.height/2 + 50> (canvas.height)) return;
-        console.log(canvas.height)
+        case 40:                                                                // KeyS -> Down
+            if (alien.y + alien.height/2 + 50> (canvas.height)) return;
             alien.y += 30;
             break;
-        case 70: //KeyF
-            disparar();
-            drawDisparo();
+        case 76:                                                                // KeyF
+        crearDisparoAlien();
             break;
-    }
-});
+        }
+    });
 
 start();
 
-// disparar balas
-// multitudes
-//multiplayer
-//teclas simultaneas
-//pantalla de seleccion
-//Arrlglasr clases
+
+/*
+ multitudes
+multiplayer
+pantalla de seleccion
+Arrlglasr clases
+*/
+
+
+
+
+/* 
+  * * * * * SE PUEDE AHORRAR CODIGO INSTANCIADO PERSONAJES Y BALAS. * * * * *
+*/
+// class Character {
+//     constructor(x,y,img){
+//         this.x = x;
+//         this.y = y;
+//         this.width = 50;
+//         this.height = 50;
+//         this.image = new Image;
+//         this.image.src = img;
+//         this.image.onload = function(){
+//             this.draw();
+//         }.bind(this)
+// }
+// draw(){
+//     ctx.drawImage(this.image, this.x, this.y, this.width,this.height);
+// }
+// }
+
+// class Human extends Character{
+//     constructor(){
+//         super (x,y,img,draw);
+//         this.bala = [];
+//     }
+// }
+
+// class Alien extends Character{
+//     constructor(){
+//         super (x,y,img,draw);
+//         this.bala = [];
+//     }
+// }
